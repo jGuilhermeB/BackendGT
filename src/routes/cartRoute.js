54fbaeabf5a58798e47f8e6350d67a60d4ab3bf8
“jGuilhermeB” <jdasilveirabarroso@gmail.com>
2025-06-03 17:00:32 -0300

    começo do prisma

--- src/routes/cartRoute.js
+++ src/routes/cartRoute.js
@@ -4,46 +4,46 @@ const router = require('express').Router();
 
 //listar itens
 router.get('/:id/itens', async (req, res) => {
-//    const {id} = req.params
-   try {
-    const result = await client.query(
-        'SELECT * FROM cart_item WHERE cart_id = $1',
-         [req.params.id]
-    )
-    res.status(200).json(result.rows)
-   } catch (error) {
+    //    const {id} = req.params
+    try {
+        const result = await client.query(
+            'SELECT * FROM cart_item WHERE cart_id = $1',
+            [req.params.id]
+        )
+        res.status(200).json(result.rows)
+    } catch (error) {
         console.log("Erro ao listar itens do carrinho", error)
-        res.status(500).json({error: 'Erro ao buscar itens'})
-   }  
- })
+        res.status(500).json({ error: 'Erro ao buscar itens' })
+    }
+})
 //cria carrinho
 router.post('/', async (req, res) => {
-    const {user_id} = req.body 
+    const { user_id } = req.body
     try {
         await client.query(
-            'INSERT INTO cart (user_id) VALUES ($1)', 
+            'INSERT INTO cart (user_id) VALUES ($1)',
             [user_id]
         )
-        res.status(201).json({message: 'Carrinho criado com sucesso'})
+        res.status(201).json({ message: 'Carrinho criado com sucesso' })
     } catch (error) {
         console.log("Erro ao criar carrinho", error)
-        res.status(500).json({erro: "Erro ao criar carrinho", details: error.message })
+        res.status(500).json({ erro: "Erro ao criar carrinho", details: error.message })
     }
- })
+})
 
- //insere itens no carrinho
+//insere itens no carrinho
 router.post('/item', async (req, res) => {
-    const {cart_id, product_id, quantity} = req.body
+    const { cart_id, product_id, quantity } = req.body
 
     if (!Number.isInteger(cart_id) || !Number.isInteger(product_id) || !Number.isInteger(quantity)) {
-        return res.status(400).json({error: "Erro no corpo da requisição"})
+        return res.status(400).json({ error: "Erro no corpo da requisição" })
     }
-     
+
     try {
 
         const carrinhoCheck = await client.query('SELECT id FROM cart WHERE id = $1', [cart_id])
         if (carrinhoCheck.rowCount === 0) {
-            return res.status(404).json({ error: 'Carrinho não encontrado'})
+            return res.status(404).json({ error: 'Carrinho não encontrado' })
         }
 
         const result = await client.query(
@@ -53,18 +53,20 @@ router.post('/item', async (req, res) => {
         res.status(201).json(result.rows)
     } catch (error) {
         console.log("Erro ao inserir item no carrinho", error)
-        res.status(500).json({erro: "Erro ao adicionar item",
-        details: error.message })
+        res.status(500).json({
+            erro: "Erro ao adicionar item",
+            details: error.message
+        })
     }
-   
- })
+
+})
 //Alterar a quantidade item carrinho
 router.put('/item/:id', async (req, res) => {
-    const {id} = req.params
-    const {quantity} = req.body
+    const { id } = req.params
+    const { quantity } = req.body
 
     if (!Number.isInteger(quantity) || quantity < 1) {
-        return res.status(400).json({ error: "quantidade inválida. Deve ser número inteiro maior que 0"})
+        return res.status(400).json({ error: "quantidade inválida. Deve ser número inteiro maior que 0" })
     }
 
     try {
@@ -74,26 +76,28 @@ router.put('/item/:id', async (req, res) => {
         )
 
         if (result.rowCount === 0) {
-            return res.status(404).json({ error: 'Item não encontrado para alterar'})
+            return res.status(404).json({ error: 'Item não encontrado para alterar' })
         }
         res.status(200).json(result.rows[0])
     } catch (error) {
         console.log("Erro ao alterar item", error)
-        res.status(500).json({erro: "Erro ao alterar item",
-        details: error.message })
+        res.status(500).json({
+            erro: "Erro ao alterar item",
+            details: error.message
+        })
     }
- })
+})
 router.delete('/:id/itens', async (req, res) => {
     const { id } = req.params;
     try {
-        await client.query(
-            'DELETE FROM cart_item WHERE cart_id = $1',
+        const result = await client.query(
+            'DELETE FROM cart_item WHERE id = $1 RETURNING *',
             [id]
         );
-        res.json({ message: 'Carrinho esvaziado com sucesso' });
+        res.status(200).json({ message: 'Carrinho esvaziado com sucesso', result: result.rows });
     } catch (error) {
-        console.error("Erro ao deletar itens do carrinho:", error);
-        res.status(500).json({ error: 'Erro ao esvaziar carrinho', details: error.message });
+        console.log("Erro ao limpar carrinho", error);
+        res.status(500).json({ error: 'Erro ao limpar carrinho', details: error.message });
     }
 });
 
